8fa40259e8736de6075c9016cc99fd051e9ebd50
Ebhota Jonathan <joe.ebh100@live.com>
2024-01-29 21:57:12 +0100

    Feature: Set up home page | Install Dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <div>
-      <p className="font-semibold">Hello World!!!</p>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md")}>
+          Auth
+        </h1>
+        <p className="text-white text-lg">A Simple Authentication Service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
