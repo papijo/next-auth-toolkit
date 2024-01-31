5bd9ba4c4ca71e4afc168d2596872410ab28a445
Ebhota Jonathan <joe.ebh100@live.com>
2024-01-31 13:53:18 +0100

    Feature: Login Form

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardFooter } from "../ui/card";
+import { BackButton } from "./back-button";
+import { Header } from "./header";
+import { Social } from "./social";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonHref,
+  backButtonLabel,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+// Start at 1:00:15
---
+++ components/auth/login-form.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { CardWrapper } from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { LoginSchema } from "@/schemas";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+
+export const LoginForm = () => {
+  // Form Schema (For Validation)
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // Form Submission
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Welcome Back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            {/* Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="john.doe@email.com" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            {/* Password */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <FormError message="" />
+            <FormSuccess message="" />
+            <Button className="w-full" type="submit">
+              Login
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
