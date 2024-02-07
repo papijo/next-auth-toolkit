0fb583ebcbedbd7a6313b20ab5a7d2cf37e02f07
Ebhota Jonathan <joe.ebh100@live.com>
2024-02-07 13:19:37 +0100

    Feature: Session and Token Functionality

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +10,32 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      console.log({ token });
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
